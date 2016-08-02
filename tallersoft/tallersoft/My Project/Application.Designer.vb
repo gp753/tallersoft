61520d01647cbc249fa3f3cd5da6ece1eb5d8067
gp753 <gp_753@hotmail.com>
2016-08-02 17:43:29 -0400

    Revert "Ultimo con startup from form1"
    This reverts commit a2beb03cb36597306796e9a30028e26d12e12e73.

--- tallersoft/tallersoft/My Project/Application.Designer.vb
+++ tallersoft/tallersoft/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.tallersoft.Form1
+            Me.MainForm = Global.tallersoft.Stock
         End Sub
     End Class
 End Namespace
