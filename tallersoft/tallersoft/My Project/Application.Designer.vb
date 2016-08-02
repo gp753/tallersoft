a2beb03cb36597306796e9a30028e26d12e12e73
hejasako <hejasako@gmail.com>
2016-08-02 16:41:30 -0400

    Ultimo con startup from form1

--- tallersoft/tallersoft/My Project/Application.Designer.vb
+++ tallersoft/tallersoft/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.tallersoft.Stock
+            Me.MainForm = Global.tallersoft.Form1
         End Sub
     End Class
 End Namespace
